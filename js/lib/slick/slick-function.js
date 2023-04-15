2f04d3df689f0fdd9aab5c4e43241b2b7bbd1fe3
MauroGith <mauro_rk_@hotmail.com>
2023-04-15 18:31:27 -0300

    update

---
+++ js/lib/slick/slick-function.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.work-boxes').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        prevArrow: '.left',
+        nextArrow: '.right',
+    });
+});
