d2a158c7905491311f037bd7f1e78c3b8a4a1d30
MauroGith <mauro_rk_@hotmail.com>
2023-04-19 00:29:20 -0300

    update

--- js/main.js
+++ js/main.js
@@ -3,3 +3,10 @@ $('.open-menu').click(() => {
     $('.open-menu').toggleClass('fa-bars')
     $('.header-menu').slideToggle()
 })
+
+$('.show-items').click(function() {
+    $(this).parent().next().find('ul').slideToggle()
+})
+$('.icon-wrapper').click(function() {
+    $(this).next().find('ul').fadeToggle()
+})
