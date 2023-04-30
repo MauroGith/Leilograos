c1647231cc6476e4200e203f14543b87922dede8
MauroGith <mauro_rk_@hotmail.com>
2023-04-30 05:13:26 -0300

    update

--- js/main.js
+++ js/main.js
@@ -16,3 +16,25 @@ $('.question').click(function() {
     $(this).find('i').toggleClass('fa-minus')
     $(this).find('i').toggleClass('fa-plus')
 })
+
+if(innerWidth < 768 ){
+    $('.chat-item').click(function(){
+        $('.chat-list').css('display', 'none')
+        $('.chat-box').css('display', 'flex')
+    })
+}
+
+$('.close-chat').click(function(){
+    $('.chat-box').css('display', 'none')
+    $('.chat-list').css('display', 'block')
+})
+
+$('.toggle-nav').click(function(){
+    
+
+    $('.dashboard-buttons nav').slideToggle()
+    $('.toggle-nav i').toggleClass('fa-chevron-down')
+    $('.toggle-nav i').toggleClass('fa-chevron-up')
+
+    $('.toggle-nav p').text() === 'Exibir Menu' ?  $('.toggle-nav p').text('Ocultar Menu') : $('.toggle-nav p').text('Exibir Menu')
+})
